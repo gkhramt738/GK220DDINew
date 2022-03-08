3732e18871ff9e7a885ef97642cfa333becc1dd0
gkhramt738 <gkhramtchenko738@g.rwu.edu>
2022-03-08 14:53:41 -0500

    completed hw5

---
+++ hw5.sql
@@ -0,0 +1,37 @@
+create table customers
+(
+    customerId int auto_increment
+        primary key,
+    firstname  varchar(20) null,
+    lastname   varchar(20) null,
+    constraint customerId
+        unique (customerId)
+);
+
+create table orders
+(
+    orderId    int auto_increment
+        primary key,
+    product    varchar(20) null,
+    cost       int         null,
+    customerId int         null,
+    constraint orderId
+        unique (orderId),
+    constraint orders_ibfk_1
+        foreign key (customerId) references customers (customerId)
+);
+
+create index customerId
+    on orders (customerId);
+
+create table products
+(
+    Id         int auto_increment
+        primary key,
+    customerId int not null,
+    orderId    int not null,
+    constraint Id
+        unique (Id)
+);
+
+
